7c1beef597809f6de04a276bc7a9fa36be83cf2d
Martin Dedek <dedekmm@gmail.com>
2025-05-29 11:14:14 +0200

    update bin/start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-# Create logs directory if it doesn't exist
-mkdir -p ~/kreslidlo/logs
+cd ~/kreslidlo
 
-echo "---- Git Pull at $(date) ----" >> ~/kreslidlo/logs/startup.log
+# Create logs directory if it doesn't exist
+mkdir -p logs
+
+echo "---- Git Pull at $(date) ----" >> logs/startup.log
 
 # Pull the latest changes
-git pull origin master >> ~/kreslidlo/logs/startup.log 2>&1
+git pull origin master >> logs/startup.log 2>&1
+
+echo "---- Git Pull complete at $(date) ----" >> logs/startup.log
+
+# Install npm dependencies
+npm install >> logs/startup.log 2>&1
 
 # Restart the Node.js app service
 sudo systemctl restart kreslidlo-app.service
+
+echo "---- Startup complete at $(date) ----" >> logs/startup.log
